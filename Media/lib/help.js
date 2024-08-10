d0d249106a07f760a8a199daafd91b5fd8a79555
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-08-10 19:19:15 +0100

    𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟑.

---
+++ Media/lib/help.js
@@ -0,0 +1,87 @@
+const prefix = global.prefa
+  const pushname = m.pushName || "No Name"
+
+const helpmenu=` 
+「 *Maria BOT* 」
+♡ۣۜۜ፝͜͜͡͡✿➤ *INFO BOT*
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Name: Maria BOT
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Version: 1.0
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Type: VPS
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Owner: 𝑨𝒚𝒖𝒔𝒉-𝒑𝒂𝒏𝒅𝒆𝒚
+  ♡ۣۜۜ፝͜͜͡͡✿➤ wa.me/919931122319
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Prefix: 「  ${prefix}  」
+ ♡ۣۜۜ፝͜͜͡͡✿➤ Total cmd: 51
+ ♡ۣۜۜ፝͜͜͡͡✿➤Menu Type: 𝑯𝒆𝒍𝒑-𝑴𝒆𝒏𝒖
+ ----------------------------------
+ 
+ ----------------------------------
+	🍁 *Coding* 🍁
+⌬${prefix}gitclone
+⌬${prefix}gdrive
+🍁 *General Commands* 🍁
+⌬${prefix}menu
+⌬${prefix}list
+⌬${prefix}hi
+⌬${prefix}support
+⌬${prefix}Maria
+⌬${prefix}rules
+⌬${prefix}info
+🍁 *Owner Commands* 🍁
+⌬${prefix}join
+⌬${prefix}bctext
+⌬${prefix}creategc
+⌬${prefix}leavegc
+🍁 *Search Commands* 🍁
+⌬${prefix}weather
+⌬${prefix}coffee
+⌬${prefix}google
+🍁 *Group Commands* 🍁
+⌬${prefix}antilink
+⌬${prefix}kick
+⌬${prefix}add
+⌬${prefix}resetlink
+⌬${prefix}gclink
+⌬${prefix}listonline
+⌬${prefix}tagall
+⌬${prefix}admins
+⌬${prefix}hidetag
+⌬${prefix}promote
+⌬${prefix}demote
+⌬${prefix}setname
+⌬${prefix}setgcpp
+🍁 *Fun Commands* 🍁
+⌬${prefix}handsomecheck
+⌬${prefix}beautifulcheck
+⌬${prefix}ship
+⌬${prefix}soulmate
+⌬${prefix}dare
+⌬${prefix}truth
+⌬${prefix}fact
+⌬${prefix}couple
+⌬${prefix}diffine
+🍁 *Weeb Commands* 🍁
+⌬${prefix}crossplay
+⌬${prefix}neko
+⌬${prefix}loli
+⌬${prefix}wallpaper
+⌬${prefix}waifu
+🍁 *Other Commands* 🍁
+⌬${prefix}iguser
+⌬${prefix}sticker
+⌬${prefix}steal
+⌬${prefix}pornhub
+⌬${prefix}calculate
+⌬${prefix}emojimix
+⌬${prefix}tts
+⌬${prefix}ssweb
+⌬${prefix}runtime
+
+
+🌹𝒎𝒐𝒓𝒆 𝒄𝒐𝒎𝒎𝒂𝒏𝒅𝒔 𝒄𝒐𝒎𝒊𝒏𝒈 𝒔𝒐𝒐𝒏
+
+『 Maria-Md 』
+   Based on: Miku, 
+   created by : Ayush pandey
+   ©️ Ayush botz inc 
+`
+module.exports = helpmenu
