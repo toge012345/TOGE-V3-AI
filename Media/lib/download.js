314fc87325edbe26e05a827c10c69e94082d171c
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-08-11 05:41:48 +0100

    𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟑.

--- Media/lib/download.js
+++ Media/lib/download.js
@@ -2,16 +2,16 @@ const prefix = global.prefa
   const pushname = m.pushName || "No Name"
 
 const codingmenu=`
-┌──⊰ _*📂download📂*_
-│⊳ 📥 ${prefix}play
-│⊳ 📥 ${prefix}ytmp3
-│⊳ 📥 ${prefix}ytmp4
-│⊳ 📥 ${prefix}igimage 
-│⊳ 📥 ${prefix}igvideo 
-│⊳ 📥 ${prefix}gitclone
-│⊳ 📥 ${prefix}pinterest
-│⊳ 📥 ${prefix}apk
-└──────────⊰
+┏━━⍟*📂download📂*⍟
+┃✺ ${prefix}play
+┃✺ ${prefix}ytmp3
+┃✺ ${prefix}ytmp4
+┃✺ ${prefix}igimage 
+┃✺ ${prefix}igvideo 
+┃✺ ${prefix}gitclone
+┃✺ ${prefix}pinterest
+┃✺ ${prefix}apk
+┗━━━━━━━━━━⊛
  `
  module.exports = codingmenu
  
