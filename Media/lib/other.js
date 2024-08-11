420acc77166ebb35c7a54f3c8c824513d5511078
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-08-11 06:09:18 +0100

    𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟑.

--- Media/lib/other.js
+++ Media/lib/other.js
@@ -2,27 +2,27 @@ const prefix = global.prefa
   const pushname = m.pushName || "No Name"
 
 const othermenu=`
-┌──⊰ _*⛩️OTHERS⛩️*_
-│⊳ 🏮 ${prefix}sticker
-│⊳ 🏮 ${prefix}qc
-│⊳ 🏮 ${prefix}smeme
-│⊳ 🏮 ${prefix}take
-│⊳ 🏮 ${prefix}toimage
-│⊳ 🏮 ${prefix}tovideo
-│⊳ 🏮 ${prefix}toaudio
-│⊳ 🏮 ${prefix}tomp3
-│⊳ 🏮 ${prefix}tovn
-│⊳ 🏮 ${prefix}togif
-│⊳ 🏮 ${prefix}tourl
-│⊳ 🏮 ${prefix}toqr
-│⊳ 🏮 ${prefix}toviewonce
-│⊳ 🏮 ${prefix}fliptext
-│⊳ 🏮 ${prefix}emojimix
-│⊳ 🏮 ${prefix}circlevideo
-│⊳ 🏮 ${prefix}google
-│⊳ 🏮 ${prefix}pinterest
-│⊳ 🏮 ${prefix}dalle
-│⊳ 🏮 ${prefix}gpt
+┏━━⍟*⛩️OTHERS⛩️*⍟
+┃✺ ${prefix}sticker
+┃✺ ${prefix}qc
+┃✺ ${prefix}smeme
+┃✺ ${prefix}take
+┃✺ ${prefix}toimage
+┃✺ ${prefix}tovideo
+┃✺ ${prefix}toaudio
+┃✺ ${prefix}tomp3
+┃✺ ${prefix}tovn
+┃✺ ${prefix}togif
+┃✺ ${prefix}tourl
+┃✺ ${prefix}toqr
+┃✺ ${prefix}toviewonce
+┃✺ ${prefix}fliptext
+┃✺ ${prefix}emojimix
+┃✺ ${prefix}circlevideo
+┃✺ ${prefix}google
+┃✺ ${prefix}pinterest
+┃✺ ${prefix}dalle
+┃✺ ${prefix}gpt
 └──────────⊰
  `
  module.exports = othermenu
