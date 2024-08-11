1a7513d340e20b4933f97ff17cfef7835c987b9a
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-08-11 06:16:48 +0100

    𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟑.

--- Config.js
+++ Config.js
@@ -16,7 +16,7 @@ global.stickername = process.env.STICKER || "𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟑"
 global.packname = 'Sticker By'
 global.author = process.env.AUTHOR || "𓆩𝐓𝐎𝐆𝐄𓆪 ✇ ◤✞𝐈𝐍𝐔𝐌𝐀𝐊𝐈",
 //console view/theme
-global.themeemoji = '🧩'
+global.themeemoji = '🛠️'
 global.wm = "𝐓𝐎𝐆𝐄_𝐁𝐎𝐓 𝐈𝐧𝐜 ✨"
 
 //theme link
