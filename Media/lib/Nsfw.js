603ccb4cf97f6a93f26ce0b71de9051b61f07b32
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-08-11 05:34:01 +0100

    𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟑.

--- Media/lib/Nsfw.js
+++ Media/lib/Nsfw.js
@@ -1,27 +1,27 @@
 const prefix = global.prefa
   const pushname = m.pushName || "No Name"
 
-const audiomenu=`┌──⊰ _*🔞NSFW 🔞*_
-│⊳ 💦  ${prefix}blowjob
-│⊳ 💦  ${prefix}cum
-│⊳ 💦  ${prefix}foot
-│⊳ 💦  ${prefix}gangbang
-│⊳ 💦  ${prefix}hentai
-│⊳ 💦  ${prefix}pussy
-│⊳ 💦  ${prefix}ass
-│⊳ 💦  ${prefix}trap
-│⊳ 💦  ${prefix}maal
-│⊳ 💦  ${prefix}ʀɪʙʙᴏɴꜱ
-│⊳ 💦  ${prefix}ʜᴀᴛꜱᴜɴᴇᴍɪᴋᴜ
-│⊳ 💦  ${prefix}ʜᴇᴀᴅʙᴀɴᴅ
-│⊳ 💦  ${prefix}ꜰᴏxɢɪʀʟ
-│⊳ 💦  ${prefix}ᴀɴɪᴍᴀʟᴇᴀʀꜱ
-│⊳ 💦  ${prefix}ʙʀᴀ
-│⊳ 💦  ${prefix}ꜱᴋɪʀᴛ
-│⊳ 💦  ${prefix}ʙʀᴇᴀꜱᴛꜱ
-│⊳ 💦  ${prefix}ᴛᴀᴛᴛᴏᴏ
-│⊳ 💦  ${prefix}ᴄʜᴀɪɴ
-└──────────⊰ 
+const audiomenu=`┏━━⍟*🔞NSFW 🔞*⍟
+┃✺  ${prefix}blowjob
+┃✺  ${prefix}cum
+┃✺  ${prefix}foot
+┃✺  ${prefix}gangbang
+┃✺  ${prefix}hentai
+┃✺  ${prefix}pussy
+┃✺  ${prefix}ass
+┃✺  ${prefix}trap
+┃✺  ${prefix}maal
+┃✺  ${prefix}ʀɪʙʙᴏɴꜱ
+┃✺  ${prefix}ʜᴀᴛꜱᴜɴᴇᴍɪᴋᴜ
+┃✺  ${prefix}ʜᴇᴀᴅʙᴀɴᴅ
+┃✺  ${prefix}ꜰᴏxɢɪʀʟ
+┃✺  ${prefix}ᴀɴɪᴍᴀʟᴇᴀʀꜱ
+┃✺  ${prefix}ʙʀᴀ
+┃✺  ${prefix}ꜱᴋɪʀᴛ
+┃✺  ${prefix}ʙʀᴇᴀꜱᴛꜱ
+┃✺  ${prefix}ᴛᴀᴛᴛᴏᴏ
+┃✺  ${prefix}ᴄʜᴀɪɴ
+┗━━━━━━━━━━⊛ 
  
  `
  module.exports = audiomenu
