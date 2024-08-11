4edabf4bcdcc0f27eea2f77f3d4d7a0d88093906
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-08-11 05:36:14 +0100

    𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟑.

--- Media/lib/SnapBlend.js
+++ Media/lib/SnapBlend.js
@@ -2,23 +2,23 @@ const prefix = global.prefa
   const pushname = m.pushName || "No Name"
 
 const reactmenu=`
-┌──⊰ _*🎐SnapBlend🎐*_
-│⊳🎀 ${prefix}shadow
-│⊳🎀 ${prefix}write
-│⊳🎀 ${prefix}smoke
-│⊳🎀 ${prefix}burnpaper
-│⊳🎀 ${prefix}romantic
-│⊳🎀 ${prefix}writeart
-│⊳🎀 ${prefix}rainboweffect
-│⊳🎀 ${prefix}smokyneon
-│⊳🎀 ${prefix}underwaterocean
-│⊳🎀 ${prefix}coffecup
-│⊳🎀 ${prefix}doublelove
-│⊳🎀 ${prefix}undergrass
-│⊳🎀 ${prefix}love
-│⊳🎀 ${prefix}narutobanner
-│⊳🎀 ${prefix}shinetext
-└──────────⊰
+┏━━⍟*🎐SnapBlend🎐*⍟
+┃✺ ${prefix}shadow
+┃✺ ${prefix}write
+┃✺ ${prefix}smoke
+┃✺ ${prefix}burnpaper
+┃✺ ${prefix}romantic
+┃✺ ${prefix}writeart
+┃✺ ${prefix}rainboweffect
+┃✺ ${prefix}smokyneon
+┃✺ ${prefix}underwaterocean
+┃✺ ${prefix}coffecup
+┃✺ ${prefix}doublelove
+┃✺ ${prefix}undergrass
+┃✺ ${prefix}love
+┃✺ ${prefix}narutobanner
+┃✺ ${prefix}shinetext
+┗━━━━━━━━━━⊛
 `
  module.exports = reactmenu
  
