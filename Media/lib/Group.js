fd20f6bc5a3f1bccaf514914e238d5fca7bca758
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-08-11 05:30:16 +0100

    𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟑.

--- Media/lib/Group.js
+++ Media/lib/Group.js
@@ -2,25 +2,25 @@ const prefix = global.prefa
   const pushname = m.pushName || "No Name"
 
 const groupmenu=`
-「 *Maria BOT* 」
-┌──⊰ _*👮🏻‍♂️GROUP👮🏻‍♂️*_
-│⊳ 🍁 ${prefix}antilink
-│⊳ 🍁 ${prefix}closetime
-│⊳ 🍁 ${prefix}opentime
-│⊳ 🍁 ${prefix}kick
-│⊳ 🍁 ${prefix}promote
-│⊳ 🍁 ${prefix}demote
-│⊳ 🍁 ${prefix}setdesc
-│⊳ 🍁 ${prefix}setppgc
-│⊳ 🍁 ${prefix}tagall
-│⊳ 🍁 ${prefix}hidetag
-│⊳ 🍁 ${prefix}totag
-│⊳ 🍁 ${prefix}group *[option]*
-│⊳ 🍁 ${prefix}editinfo
-│⊳ 🍁 ${prefix}gclink
-│⊳ 🍁 ${prefix}revoke
-│⊳ 🍁 ${prefix}listonline
-└──────────⊰
+「 *TOGE-MD-V3* 」
+┏━━⍟*👮🏻‍♂️GROUP👮🏻‍♂️*⍟
+┃✺ ${prefix}antilink
+┃✺ ${prefix}closetime
+┃✺ ${prefix}opentime
+┃✺ ${prefix}kick
+┃✺ ${prefix}promote
+┃✺ ${prefix}demote
+┃✺ ${prefix}setdesc
+┃✺ ${prefix}setppgc
+┃✺ ${prefix}tagall
+┃✺ ${prefix}hidetag
+┃✺ ${prefix}totag
+┃✺ ${prefix}group
+┃✺ ${prefix}editinfo
+┃✺ ${prefix}gclink
+┃✺ ${prefix}revoke
+┃✺ ${prefix}listonline
+┗━━━━━━━━━━⊛
  `
  module.exports = groupmenu
  
