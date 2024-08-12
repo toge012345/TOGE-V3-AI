94d5b47206e78746fb148f5ac78f6028049e91ec
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-08-12 06:28:58 +0100

    𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟑.

---
+++ Gallery/lib/download.js
@@ -0,0 +1,17 @@
+const prefix = global.prefa
+  const pushname = m.pushName || "No Name"
+
+const codingmenu=`
+┏━━⍟*📂download📂*⍟
+┃✺ ${prefix}play
+┃✺ ${prefix}ytmp3
+┃✺ ${prefix}ytmp4
+┃✺ ${prefix}igimage 
+┃✺ ${prefix}igvideo 
+┃✺ ${prefix}gitclone
+┃✺ ${prefix}pinterest
+┃✺ ${prefix}apk
+┗━━━━━━━━━━⊛
+ `
+ module.exports = codingmenu
+ 
---
+++ Gallery/lib/other.js
@@ -0,0 +1,29 @@
+const prefix = global.prefa
+  const pushname = m.pushName || "No Name"
+
+const othermenu=`
+┏━━⍟*⛩️OTHERS⛩️*⍟
+┃✺ ${prefix}sticker
+┃✺ ${prefix}qc
+┃✺ ${prefix}smeme
+┃✺ ${prefix}take
+┃✺ ${prefix}toimage
+┃✺ ${prefix}tovideo
+┃✺ ${prefix}toaudio
+┃✺ ${prefix}tomp3
+┃✺ ${prefix}tovn
+┃✺ ${prefix}togif
+┃✺ ${prefix}tourl
+┃✺ ${prefix}toqr
+┃✺ ${prefix}toviewonce
+┃✺ ${prefix}fliptext
+┃✺ ${prefix}emojimix
+┃✺ ${prefix}circlevideo
+┃✺ ${prefix}google
+┃✺ ${prefix}pinterest
+┃✺ ${prefix}dalle
+┃✺ ${prefix}gpt
+└──────────⊰
+ `
+ module.exports = othermenu
+ 
