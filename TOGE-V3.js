f107d923b304794a133ccb901dd0a339c5f4429d
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-08-30 02:18:49 +0100

    𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟑.

--- TOGE-V3.js
+++ TOGE-V3.js
@@ -1,6 +1,6 @@
 const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getAggregateVotesInPollMessage,getContentType, delay, decodeJid } = require('@whiskeysockets/baileys')
 const { SendGroupInviteMessageToUser } = require("@queenanya/invite")
-const config = require("./Config")
+const Config = require("./Config")
 const os = require('os')
 const fs = require('fs')
 const mathjs = require('mathjs')
